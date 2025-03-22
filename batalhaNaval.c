3036e2624dfbb10ee29f8c073b3c5eccb1404d97
IuriMarreiro <iurimarreironunes@gmail.com>
2025-03-22 02:45:50 +0000

    desafio nivel mestre concluido batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -22,38 +22,56 @@ int main () {
     }
     printf("\n");
 
-//Declarando na matriz o navio na horizontal
-//Loop For para registrar o navio
 
+
+//Declando na matriz uma figura geometrica "cone"
+//Loop For aninhado com condicionais para gerar o cone na matriz
+
+    for (i = 8; i >= 6; i--)
+    {   for (j = 1; j <= 5; j++)
+        {
+            if (i == 7)
+            {
+                for (j = 2; j <= 4; j++) {
+                    tabuleiro[i][j] = 7;
+                } 
+            } 
+            else {
+                if (i == 6) break;
+                tabuleiro[i][j] = 7;
+            }
+        }
+        if (i == 6) {
+            j = 3;
+            tabuleiro[i][j] = 7;
+        }
+    }
+    
+    
+//Declando na matriz uma figura geometrica "losango"
+//Um loop For simples para declarar uma parte do losango e um For aninhado para completar a figura
+
+    for (i = 2, j = 6; i <= 6; i++)
+    {
+        tabuleiro [i][j] = 3;
+        tabuleiro [j - 2][i + 2] = 3;
+    }
     for (i = 3; i <= 5; i++)
-    {
-        tabuleiro[2][i] = 7;
-    }
-
-//Declarando na matriz o navio na vertical
-//Loop For para registrar o navio
-
-    for (j = 6; j <= 8; j++)
-    {
-        tabuleiro[j][7] = 7;
-    }
-
-//Declando na matriz o navio na diagonal primaria
-//Loop For com duas variveis inicializadas com condicionais para registrar o navio
-
-    for (i = 3, j = 1; i <= 5 && j <= 3; i++, j++)
-    {
-        tabuleiro[i][j] = 7;
+    {   for (j = 5; j <= 7; j++)
+        {
+            tabuleiro [i][j] = 3;
+        }
     }
     
-//Declando na matriz o navio na diagonal secundaria
-//Loop For com duas variveis inicializadas com condicionais para registrar o navio
+//Declando na matriz uma figura geometrica "cruz"
+//Loop For para gerar a cruz com inverção das variaveis
 
-    for (i = 1, j = 8; i <= 3 && j >= 6; i++, j--)
-    {
-        tabuleiro[i][j] = 7;
-    }
-    
+        for (i = 0, j = 2; i <= 4; i++)
+        {
+            tabuleiro [i][j] = 1;
+            tabuleiro [j][i] = 1;
+        }
+        
 //Código para imprimir o tabuleiro no console
 //Matriz do tabuleiro é impresso no console por meio do loop For aninhado
 
